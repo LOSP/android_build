72bf9396de5ec45b8f8bfc588b7086b0685864fd
PeterCxy <xqsx43cxy@126.com>
2014-07-08 15:35:11 +0800

    javac.mk: set target to javac 1.7

--- core/combo/javac.mk
+++ core/combo/javac.mk
@@ -10,14 +10,14 @@
 # Whatever compiler is on this system.
 ifeq ($(BUILD_OS), windows)
     COMMON_JAVAC := development/host/windows/prebuilt/javawrap.exe -J-Xmx256m \
-        -target 1.5 -Xmaxerrs 9999999
+        -target 1.7 -Xmaxerrs 9999999
 else
-    COMMON_JAVAC := javac -J-Xmx512M -target 1.5 -Xmaxerrs 9999999
+    COMMON_JAVAC := javac -J-Xmx512M -target 1.7 -Xmaxerrs 9999999
 endif
 
 # Eclipse.
 ifeq ($(CUSTOM_JAVA_COMPILER), eclipse)
-    COMMON_JAVAC := java -Xmx256m -jar prebuilt/common/ecj/ecj.jar -5 \
+    COMMON_JAVAC := java -Xmx256m -jar prebuilt/common/ecj/ecj.jar -7 \
         -maxProblems 9999999 -nowarn
     $(info CUSTOM_JAVA_COMPILER=eclipse)
 endif
@@ -25,7 +25,7 @@ endif
 # OpenJDK.
 ifeq ($(CUSTOM_JAVA_COMPILER), openjdk)
     # We set the VM options (like -Xmx) in the javac script.
-    COMMON_JAVAC := prebuilt/common/openjdk/bin/javac -target 1.5 \
+    COMMON_JAVAC := prebuilt/common/openjdk/bin/javac -target 1.7 \
         -Xmaxerrs 9999999
     $(info CUSTOM_JAVA_COMPILER=openjdk)
 endif
